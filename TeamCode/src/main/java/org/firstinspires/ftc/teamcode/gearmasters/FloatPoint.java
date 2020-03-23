7aaa41e3b9b2c0bb222070853d9584ae264a3468
ngaik999 <ngaik999@yahoo.com>
2020-03-22 21:45:10 -0400

    Skystone 5.3

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gearmasters/FloatPoint.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.gearmasters;
+
+
+public class FloatPoint {
+    public double x;
+    public double y;
+    public FloatPoint(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
